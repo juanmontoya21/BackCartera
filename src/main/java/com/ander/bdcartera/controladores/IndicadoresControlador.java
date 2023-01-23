f4cad4d71bfff7897a3a798d0387d3d67baa1e5c
Practicante Manufactura  Juan Sebastian Montoya Vasquez <p.manufactura06@andercol.com.co>
2023-01-23 10:44:31 -0500

    se arregla el getmaping indicadores

--- src/main/java/com/ander/bdcartera/controladores/IndicadoresControlador.java
+++ src/main/java/com/ander/bdcartera/controladores/IndicadoresControlador.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(name = "api/ander/indicadores")
+@RequestMapping("api/ander/indicador")
 public class IndicadoresControlador {
     @Autowired
     IndicadoresServicioImplementacion indicadoresServicio;
